feaff8c8a9d2907012607fe40e87ed1d5c0736f8
Abhishek Sahu <kalisahu752@gmail.com>
2024-06-25 22:41:21 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { Button } from "@/components/ui/button";
-import Tasks from "@/components/todolist-client/tasks";
 
+import { signInAction } from "../../actions/auth-action";
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       TODO LIST
-      <Button>HI</Button>
-      <Tasks/>
+
+      <form action={signInAction}>
+      <Button>LogIn</Button>
+      </form>
     </main>
   );
 }
