460d25c910bd8be2b40f8a347a8e3c299b17a018
Abhishek Sahu <kalisahu752@gmail.com>
2024-06-25 23:07:18 +0530

    5th commit

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,7 +16,6 @@ import type {
 } from "convex/server";
 import type * as authAdapter from "../authAdapter.js";
 import type * as http from "../http.js";
-import type * as tasks from "../tasks.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -29,7 +28,6 @@ import type * as tasks from "../tasks.js";
 declare const fullApi: ApiFromModules<{
   authAdapter: typeof authAdapter;
   http: typeof http;
-  tasks: typeof tasks;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
